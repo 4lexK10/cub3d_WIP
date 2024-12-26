9e474a78b64146bab32167adfc07630832c1e9f5
Lina Boumahdi <lboumahd@mi-r5-p1.s19.be>
2024-12-27 00:36:29 +0100

    WIP

--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -6,35 +6,42 @@
 /*   By: lboumahd <lboumahd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 20:50:36 by lboumahd          #+#    #+#             */
-/*   Updated: 2024/12/26 23:59:55 by lboumahd         ###   ########.fr       */
+/*   Updated: 2024/12/27 00:35:57 by lboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include " cub3d.h"
+#include "cub3d.h"
+
 // check data 
     //inputs 
     //configurations 
     //
 
 //init data
-
-
-void	create_map(t_data *data, int fd)
+void	parse_map(t_data *data, int fd)
 {
 	char *line;
+    int i;
+
+    i = 0;
 	line = get_next_line(fd);
+    //check first inputs RGB + texture
+        //get rid of white space 
+        //get rid of the 6 first lines
 	while(line)
 	{
-		//check first inputs RGB + texture
-	
+        //join thelines with a delimiter of choice 
+        if(line[i] == '\n')
+     
 		line = get_new_line(fd);
 	}
+       data->map = ft_split 
 }
 void	init_parsing(t_data *data, int fd)
 {
 	//check extensions
 	//createnap
-	create_map(data, fd);
+	parse_map(data, fd);
 }
 void	init_data(t_data *data)
 {
